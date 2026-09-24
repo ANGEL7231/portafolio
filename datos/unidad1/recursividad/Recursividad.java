dad3952d3f87b2a341113ff47956f3b9c543a2f1
Angel_Nolasco <1225100764.ajnr@gmail.com>
2026-09-24 15:51:04 -0600

    *Actualizacion*Mi primer metodo de recursividad, imprime 10 veces saludo a una persona

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -2,18 +2,21 @@ package datos.unidad1.recursividad;
 
 public class Recursividad {
 
-	public static void saludo(int total) {
+	public static void saludo(int total, String nombre) {
 		if (total<=0) //Condicion Base
 			return;
 		
 		else{	//Condicion Recursiva o repetitiva
-			System.out.println("Hola");
-			saludo(total-1);
+			System.out.println("Hola"+ " " + nombre);
+			saludo(total-1, nombre);
 		}
 	}
 
 	public static void main(String[] args){
-		saludo(10);
+		String nombre = "Noemi";
+		saludo(10, nombre);
+		
 	
 	}
 }
+
