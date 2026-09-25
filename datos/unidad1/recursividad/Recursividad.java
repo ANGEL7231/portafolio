064c33b5de6e7c345aaa98da65524e0517403829
Angel_Nolasco <1225100764.ajnr@gmail.com>
2026-09-25 14:51:47 -0600

    Recursividad ejercicio practico. Suma

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -12,22 +12,48 @@ public class Recursividad {
 		}
 	}
 	
+	/**
+	**Funcion que realiza cuenta regresiva de acuerdo a un
+	*valor entero
+	*Fecha: 25 Septiembre
+	*Autor: Angel Nolasco
+	*/
+	
 	public static void cuentaRegresiva(int n){
 		if(n<1){
 			return;
 		
 		} else {
 			
-			System.out.print(n + " ");
+			System.out.println(n + " ");
 			cuentaRegresiva(n-1);
 		}
 	
 	}
 	
+
+	/**
+	**Funcion que realiza suma de un arreglo con recursividad
+	**Fecha: 25 Septiembre
+	*Autor: Angel Nolasco
+	*/
+	
+	public static int sumaRecursiva(int[] datos, int tam){
+		if(tam<0){
+			return 0;
+		}else{
+			return datos[tam]+sumaRecursiva(datos, tam-1);
+		}
+	} 
+
 	public static void main(String[] args){
 		String nombre = "Noemi";
 		saludo(10, nombre);
 		cuentaRegresiva(100);
+		int[] datos = {3,4,5,6,7,8};
+		
+		int suma = sumaRecursiva(datos, datos.length - 1);
+		System.out.println("La suma del arreglo es: " + suma);
 		
 	
 	}
